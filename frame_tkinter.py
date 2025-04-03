6c3df99703e82096ba688157317c3bf6d7062b52
Pammela Vicenti <pammelapvr@gmail.com>
2025-04-03 19:51:31 -0300

    Projeto atualizado

--- frame_tkinter.py
+++ frame_tkinter.py
@@ -1,5 +1,6 @@
 ''' Criando frames na Tela usando o customtkinter'''
 
+import os 
 import customtkinter as ctk # Importando a biblioteca 
 import cv2
 from PIL import Image, ImageTk
@@ -7,9 +8,12 @@ from PIL import Image, ImageTk
 
 janela = ctk.CTk() # Criar a nossa janela
 
+# Caminho para o logotipo
+image_path = r"C:\Users\pamme\OneDrive\Documentos\IFES.jpg"
+
 
 # Configurando a janela principal
-janela.title("app teste")
+janela.title("Sistema Supervisório - Correia Transportadora")
 janela.geometry("800x700")
 janela.maxsize(width=900, height=550)
 janela.minsize(width=500, height=300)
@@ -29,8 +33,13 @@ frame4 = ctk.CTkFrame(master=janela, width=350, height=200, fg_color="white", co
 frame4.place(x=410, y=300)
 
 # Criando a Label dentro do frame1 onde a webcam será exibida
-video_label = ctk.CTkLabel(frame1, text="", width=350, height=200)
-video_label.place(x=0, y=0)  # Garante que a Label ocupa toda a área do frame
+video_label = ctk.CTkLabel(frame1, text="", width=330, height=180)
+video_label.place(x=10, y=10)  # Garante que a Label ocupa toda a área do frame
+
+# Criando a Label dentro do frame2 onde a webcam será exibida
+video_label2 = ctk.CTkLabel(frame2, text="", width=330, height=180)
+video_label2.place(x=10, y=10)
+
 
 # Captura da webcam (0 = webcam padrão)
 cap = cv2.VideoCapture(0)   
@@ -44,6 +53,9 @@ def exibir_frame():
         
         video_label.configure(image=img)
         video_label.image = img  # Manter referência da imagem
+
+        video_label2.configure(image=img)
+        video_label2.image = img  # Manter referência
         
     janela.after(10, exibir_frame)  # Atualizar a cada 10ms
 
@@ -57,6 +69,28 @@ def fechar():
     janela.destroy()
 
 janela.protocol("WM_DELETE_WINDOW", fechar)
+
+# Função para adicionar o logo
+def adicionar_logo(tela):
+    frame_logo = ctk.CTkFrame(tela)
+    frame_logo.pack(fill="x", pady=2, anchor="nw")
+
+    try:
+        if not os.path.exists(image_path):
+            raise FileNotFoundError(f"Imagem não encontrada no caminho: {image_path}")
+
+        img = Image.open(image_path).resize((180, 100))
+        logo_image = ImageTk.PhotoImage(img)
+
+        logo_label = ctk.CTkLabel(frame_logo, image=logo_image, text="") 
+        logo_label.image = logo_image 
+        logo_label.pack(anchor="nw")
+    except Exception as e:
+        print(f"Erro ao carregar a imagem: {e}")
+        logo_label = ctk.CTkLabel(frame_logo, text="Logo não encontrada", font=("Helvetica", 10), text_color="red")
+        logo_label.pack(anchor="nw")
+
+adicionar_logo(janela)
 
 # Iniciar o loop principal
 janela.mainloop()
